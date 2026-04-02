9971a162752d9d8d18edbbc054df4d749fdb4f67
Jony Sagor <hsagor703@gmail.com>
2026-04-02 11:13:31 +0600

    dashboard protected done

--- src/app/(dashboardLayout)/layout.jsx
+++ src/app/(dashboardLayout)/layout.jsx
@@ -1,9 +1,30 @@
-
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import DashboardNavbar from "@/component/(dashboardComponent)/DashboardNavbar/Navbar";
 import DashboardSidebar from "@/component/(dashboardComponent)/dashboardSidebar/SideBar";
+import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
-const DashboardLayout = ({children}) => {
+const DashboardLayout = ({ children }) => {
+  const session = useSession();
+  const path = usePathname();
+  const router = useRouter();
+  useEffect(() => {
+    if (session?.status === "unauthenticated") {
+      router.push(`/login?callbackUrl=${path}`);
+    }
+  }, [path, router, session.status]);
+
+  if (session?.status === "loading") {
+    return (
+      <span className="text-center loading loading-spinner text-success"></span>
+    );
+  }
+
+  if (session?.status === "authenticated") {
+    return router.push("/dashboard");
+  }
+
   return (
     <section className=" ">
       <div className="">
@@ -11,13 +32,13 @@ const DashboardLayout = ({children}) => {
           <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
             {/* Navbar */}
-            <DashboardNavbar/>
+            <DashboardNavbar />
             {/* Page content here */}
             <div className="p-4">{children}</div>
           </div>
 
           {/* sidebar  */}
-          <DashboardSidebar/>
+          <DashboardSidebar />
         </div>
       </div>
     </section>
