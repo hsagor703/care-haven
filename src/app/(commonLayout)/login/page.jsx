08a4662af0010c9dcaa3fdb217685db43d80544a
Jony Sagor <hsagor703@gmail.com>
2026-04-18 07:23:40 +0600

    some issue solve

--- src/app/(commonLayout)/login/page.jsx
+++ src/app/(commonLayout)/login/page.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import GoogleLogin from "@/component/buttons/GoogleLogin";
+import { Suspense } from "react";
 const LoginPage = () => {
   const router = useRouter();
   const params = useSearchParams();
@@ -33,59 +34,61 @@ const LoginPage = () => {
     }
   };
   return (
-    <div className="min-h-screen flex items-center justify-center  px-4">
-      <div className="card w-full max-w-md bg-color shadow-2xl">
-        <div className="card-body">
-          <h2 className="text-3xl font-bold text-center text-emerald-700">
-            Login
-          </h2>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="min-h-screen flex items-center justify-center  px-4">
+        <div className="card w-full max-w-md bg-color shadow-2xl">
+          <div className="card-body">
+            <h2 className="text-3xl font-bold text-center text-emerald-700">
+              Login
+            </h2>
 
-          <form onSubmit={handleLogin} className="space-y-4 mt-4">
-            {/* Email */}
-            <div>
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter your email"
-                className="input input-bordered w-full focus:input-success"
-                required
-              />
-            </div>
+            <form onSubmit={handleLogin} className="space-y-4 mt-4">
+              {/* Email */}
+              <div>
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  className="input input-bordered w-full focus:input-success"
+                  required
+                />
+              </div>
 
-            {/* Password */}
-            <div>
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                className="input input-bordered w-full focus:input-success"
-                required
-              />
-            </div>
+              {/* Password */}
+              <div>
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  className="input input-bordered w-full focus:input-success"
+                  required
+                />
+              </div>
 
-            {/* Button */}
-            <Button>{"Login"}</Button>
-          </form>
-          {/* google btn */}
-          <GoogleLogin />
-          <p className="text-center mt-4">
-            Don't have an account?
-            <Link
-              href={`/register?callbackUrl=${params.get("callbackUrl") || "/"}`}
-              className="text-emerald-700 font-semibold"
-            >
-              Register
-            </Link>
-          </p>
+              {/* Button */}
+              <Button>{"Login"}</Button>
+            </form>
+            {/* google btn */}
+            <GoogleLogin />
+            <p className="text-center mt-4">
+              Don't have an account?
+              <Link
+                href={`/register?callbackUrl=${params.get("callbackUrl") || "/"}`}
+                className="text-emerald-700 font-semibold"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
