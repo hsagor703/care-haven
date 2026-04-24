13a8d713b57134ac1e7ff51db1cc1371ff40c286
Jony Sagor <hsagor703@gmail.com>
2026-04-24 07:56:38 +0600

    ok last update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
   // if (session?.status === "loading") return null;
   return (
     <div>
-      <div className="navbar bg-black/50 backdrop-blur-md shadow-sm pl-4 md:pl-0 fixed z-10">
+      <div className="navbar bg-black/50 backdrop-blur-md shadow-sm pl-2 md:pl-0 fixed z-10">
         <div className="navbar-start">
           <div className="dropdown mr-2 md:mr-0">
             <div tabIndex={0} role="button" className=" lg:hidden ">
